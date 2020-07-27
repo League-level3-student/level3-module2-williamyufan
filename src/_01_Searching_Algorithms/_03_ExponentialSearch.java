f857cd7c55089051d3326245413d0dcd0f0226a7
yuanmin <yuanmin@192.168.1.4>
2020-07-27 11:29:42 -0700

    do algorithms

--- src/_01_Searching_Algorithms/_03_ExponentialSearch.java
+++ src/_01_Searching_Algorithms/_03_ExponentialSearch.java
@@ -47,32 +47,8 @@ boolean y=true;
 		//		half of the counter
 		//		minimum
 		//		the value to be found
-		if(end>=start) {
-			
-			
-			//3. create an integer called mid and set it equal
-			//   to the half way point between start and end
-			int mid=(end-start)/2+start;
-            //4. if the array element at mid is equal to value
-            //   then return mid
-			if(array[mid]==value) {
-				return mid;
-			}
-            //5. if the array element at mid is greater than value
-            //   then return the value returned from a call to the 
-            //   binarySearch method. Pass in start and mid - 1
-            //   for the end variable.
-            if(array[mid]>value) {
-            	return binarySearch(array, start, mid-1, value);
-            	
-            }
-            //6. return the value returned from a call to the binarySearch
-            //   method. Use mid + 1 as the start, and pass in end.
-            return binarySearch(array, mid+1, end, value);
-		}
-        //7. return -1 because the value was not found
-        return -1;
+		int q=_01_BinarySearch.binarySearch(array, counter/2, minimum, value);
 		//10. return the result
-		return 0;
+		return q;
 	}
 }
--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -23,22 +23,30 @@ class _04_SearchTest {
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
-String[] words= {"duck", "goose", "roar", "quad", "precious"};
+int[] words= {0, 1, 2, 3, 4};
 		
-		assertEquals(0, _01_BinarySearch.binarySearch(, 0, 0, 0));
-		assertEquals(1, _01_BinarySearch.binarySearch(null, 0, 0, 0));
-		assertEquals(-1, _01_BinarySearch.binarySearch(null, 0, 0, 0));
+		assertEquals(0, _01_BinarySearch.binarySearch(words, 0, words.length-1, 0));
+		assertEquals(1, _01_BinarySearch.binarySearch(words, 0, words.length-1, 1));
+		assertEquals(-1, _01_BinarySearch.binarySearch(words, 0, words.length-1, -5));
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		int[] numbers= {0, 1, 2, 3, 4};
+		assertEquals(0, _02_InterpolationSearch.interpolationSearch(numbers, 0));
+		assertEquals(2, _02_InterpolationSearch.interpolationSearch(numbers, 2));
+		assertEquals(-1, _02_InterpolationSearch.interpolationSearch(numbers, -1));
 	}
 	
 	@Test
 	public void testExponentialSearch() {
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
+		int[] nums= {0, 1, 2, 3, 4};
+		assertEquals(0, _03_ExponentialSearch.exponentialSearch(nums, 0));
+		assertEquals(3, _03_ExponentialSearch.exponentialSearch(nums, 3));
+		assertEquals(-1, _03_ExponentialSearch.exponentialSearch(nums, -1));
 	}
 }
